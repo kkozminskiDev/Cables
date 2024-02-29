bc71d5b481b25f6f3d13bed8de3903300b778e33
Krzysztof Kozminski <krzysztof.kozminski4@gmail.com>
2024-02-29 23:24:02 +0100

    GoodTrack

--- ProblemB - Kable/ProblemB - Kable.cpp
+++ ProblemB - Kable/ProblemB - Kable.cpp
@@ -1,20 +1,55 @@
-﻿// ProblemB - Kable.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int D;
+    cin >> D;
+    double* results = new  double[D];
+
+    for (int i = 0; i < D; ++i) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<double> spools(N);
+        for (int j = 0; j < N; ++j) {
+            cin >> spools[j];
+        }
+
+        sort(spools.begin(), spools.end(), greater<double>());
+
+        double maxLength = 0.0;
+        double left = 0.0, right = spools[0];
+        while (right - left > 0.005) {
+            double mid = (left + right) / 2.0;
+            int count = 0;
+            for (int k = 0; k < N; ++k) {
+                count += static_cast<int>(spools[k] / mid);
+            }
+            if (count >= K) {
+                maxLength = max(maxLength, mid);
+                left = mid;
+            }
+            else {
+                right = mid;
+            }
+        }
+
+        if (maxLength >= 0.01) {
+            results[i] = maxLength;
+        }
+        else {
+            results[i] = 0.00;
+        }
+    }
+
+    for (int i = 0; i < D; i++) {
+        cout << fixed;
+        cout.precision(2);
+        cout << results[i] << endl;     
+    }
+
+    return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
